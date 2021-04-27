a87c657b8d998eee319c65aa1c9fa9393ef624ca
Thomas Fritzsche <thomas.fritzsche@iav.de>
2021-04-27 11:17:04 +0200

    add debug logger

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/ics.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/ics.py
@@ -172,6 +172,9 @@ class Source:
             )
             return "error"
 
+        # log the result. for debugging
+        _LOGGER.debug(r.text)
+
         return self._convert(r.text)
 
     def fetch_file(self, file):
